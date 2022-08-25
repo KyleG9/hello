554932caa79b58b95bc2332d8f61d82ff2facfd7
KyleG9 <111709882+KyleG9@users.noreply.github.com>
2022-08-25 17:50:08 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author: Kyle Gann and Benjamin wormsley
+ *          kgann2@unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hegccllo world program in C
+ *
+ -------
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Kyle Gann and Benjamin wormsley \n");
+
+  return 0;
+}
